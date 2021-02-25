c7ee3e375130ff4074f2422403585b58d5f93a13
Neil Oribello <neil.oribello@gmail.com>
2021-02-25 00:09:21 +0000

    Second Commit: AJAX GET/POST requests. Submitting posts and displaying feed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,7 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() { 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
+  const data = []
 
   const renderTweets = function(tweets) {
     // loops through tweets
@@ -42,6 +18,16 @@ $(document).ready(function() {
       $("#tweets-container").append($tweet);
     }
   }
+
+  const loadTweets = function() {
+    $.ajax("/tweets", {
+      method: "GET",
+      success: function(res) {
+        renderTweets(res);
+      }
+    });
+  };
+  loadTweets();
 
   const createTweetElement = function(tweet) {
     let $tweet = /* Your code for creating the tweet element */
@@ -81,5 +67,13 @@ $(document).ready(function() {
   }
   renderTweets(data);
 
-  
+  $("#tweet-form").on('submit', function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $(this).serialize()
+    })
+    .then(console.log(this))
+  })
 });
